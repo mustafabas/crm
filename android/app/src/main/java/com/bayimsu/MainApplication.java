5b37ae2e24f81eb6beafb5815421ccb6f81d4f9d
bilal oguz <bilalmarifet@gmail.com>
2020-02-08 19:02:37 +0300

    yeter

--- android/app/src/main/java/com/bayimsu/MainApplication.java
+++ android/app/src/main/java/com/bayimsu/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // <-- Add this line
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           packages.add(new RNFirebaseMessagingPackage());
+          packages.add(new RNFirebaseNotificationsPackage());
           return packages;
         }
 
