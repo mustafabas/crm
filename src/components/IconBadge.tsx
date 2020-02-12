c597b24dbc74bf112b0cb08cfd398df02587b2c1
bilal oguz <bilalmarifet@gmail.com>
2020-02-13 00:16:26 +0300

    :

---
+++ src/components/IconBadge.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, Icon } from "native-base";
+
+
+function IconWithBadge({ badgeCount}) {
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Icon name="ios-profile"   />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  export function HomeIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props}  />;
+  }
