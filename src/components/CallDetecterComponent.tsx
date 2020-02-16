73beec8492f73170c75fbf5f3c83abdb55c19174
mustafabasm <mustafabas2189@gmail.com>
2020-02-16 21:59:36 +0300

    s

--- src/components/CallDetecterComponent.tsx
+++ src/components/CallDetecterComponent.tsx
@@ -28,11 +28,40 @@ export default class HomeComponent extends Component {
         updatedCallStates.push(event + ' - ' + number)
         var previousDS = this.state.ds
         this.setState({ callStates:  updatedCallStates});
+
+        if (event === 'Disconnected') {
+          // Do something call got disconnected
+          } 
+          else if (event === 'Connected') {
+          // Do something call got connected
+          // This clause will only be executed for iOS
+          } 
+          else if (event === 'Incoming') {
+          // Do something call got incoming
+          console.log("incoming number", number);
+
+          }
+          else if (event === 'Dialing') {
+          // Do something call got dialing
+          // This clause will only be executed for iOS
+          } 
+          else if (event === 'Offhook') {
+          //Device call state: Off-hook. 
+          // At least one call exists that is dialing,
+          // active, or on hold, 
+          // and no calls are ringing or waiting.
+          // This clause will only be executed for Android
+          console.log("incoming number", number);
+          }
+          else if (event === 'Missed') {
+            // Do something call got missed
+            // This clause will only be executed for Android
+          }
       },
-      false, // if you want to read the phone number of the incoming call [ANDROID], otherwise false
+      true, // if you want to read the phone number of the incoming call [ANDROID], otherwise false
       ()=>{}, // callback if your permission got denied [ANDROID] [only if you want to read incoming number] default: console.error
       {
-        title: 'Phone State Permission',
+        title: 'Telefon Durum İzni',
         message: 'This app needs access to your phone state in order to react and/or to adapt to incoming calls.'
       } // a custom permission request message to explain to your user, why you need the permission [recommended] - this is the default one
     )
