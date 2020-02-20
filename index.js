3324162bedb851c38477c802da7e36aee9fab01d
bilal oguz <bilalmarifet@gmail.com>
2020-02-20 09:37:08 +0300

    s

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import HomeComponent from './src/components/CallDetecterComponent';
 import {name as appName} from './app.json';
+import AppNew from './AppNew'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppNew);
